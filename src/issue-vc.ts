264ea2770fbe72998f6b2c4c6b1770be0d315ff4
Pablo Cibraro <722557+pcibraro@users.noreply.github.com>
2022-05-09 17:03:17 -0300

    Update issue-vc.ts
    Added missing fields in VC

--- src/issue-vc.ts
+++ src/issue-vc.ts
@@ -2,19 +2,26 @@ import { agent } from './veramo/setup'
 
 async function main() {
   
+   const expirationDate = new Date();
+    expirationDate.setFullYear(expirationDate.getFullYear() + 3);
+
     const vc = await agent.createVerifiableCredential({
         credential: { 
-            issuer: 'did:ethr:rinkeby:0x0366e2f069c8312e16dd7225c8e18c10145520ac8ea1405a7adea10e63ad544c27',
             '@context': ['https://w3id.org/citizenship/v1'],
+            issuer: {
+                id: 'did:ethr:rinkeby:0x0366e2f069c8312e16dd7225c8e18c10145520ac8ea1405a7adea10e63ad544c27'
+            },
             type: ['VerifiableCredential'],
             credentialSubject: {
                 type: ["PermanentResident"],
                 id: 'did:ethr:rinkeby:0x0366e2f069c8312e16dd7225c8e18c10145520ac8ea1405a7adea10e63ad544c27',
                 givenName: 'Pablo',
                 familyName: 'Cibraro'
-            }
+            },
+            expirationDate: expirationDate.toJSON(),
+            id: 'did:ethr:rinkeby:0x0366e2f069c8312e16dd7225c8e18c10145520ac8ea1405a7adea10e63ad544c27'
         },
-        save: false,
+        save: true,
         proofFormat: 'lds',
     });
   
